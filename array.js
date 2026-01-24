34bf236d3544670ea84fe2a1e2bddb68763d59ff
Aminul islam <aminurrohman98@gmail.com>
2026-01-24 20:48:19 +0000

    I practiced about Declare an array

---
+++ array.js
@@ -0,0 +1,13 @@
+// JS array task
+// 1. Declare an array
+
+// Declare an array with 5 elements containing fruits
+// console log the 3rd index element
+// change the value of the 2nd index element to jambura
+// console log the final array
+
+// -------------Solution of Declare an array--------------
+// let fruits = ["Mango", "Apple", "Tangerine", "Strawberry", "Banana"];
+// console.log(fruits[3]);
+// fruits[2] = "Jambura";
+// console.log(fruits);
