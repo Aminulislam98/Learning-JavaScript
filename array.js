13250d120c9d1b23f07aebc9f725e88e245ca0e6
Aminul islam <aminurrohman98@gmail.com>
2026-01-24 20:55:19 +0000

    I practiced about , how to remove any data from array and hoe to removed from array

--- array.js
+++ array.js
@@ -11,3 +11,20 @@
 // console.log(fruits[3]);
 // fruits[2] = "Jambura";
 // console.log(fruits);
+
+// -------------Solution of Add or remove elements--------------
+
+// Add or remove elements
+// Declare an array of 3 tourist destinations
+// Add a new tourist destination to your tourist array
+// Add two more to your array
+// Remove the last tourist destination you have added
+// display the final array as output
+
+// let place = ["Bangladesh", "England", "Canada"];
+// place.push("America");
+// console.log(place);
+// place.push("Portugal", "Dubai");
+// console.log(place);
+// place.pop();
+// console.log(place);
