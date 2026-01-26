54318541e3d1567b2e6ea355f484a680a6060cb3
Aminul islam <aminurrohman98@gmail.com>
2026-01-26 20:17:34 +0000

    I have practiced the exercise of module 15

--- array.js
+++ array.js
@@ -132,3 +132,221 @@ Find all the even numbers from 78 to 98.
 //   }
 //   array++;
 // }
+/***
+ * 
+ * 
+
+Generate a multiplication table for number 9
+
+ */
+
+/*programming hero*/
+
+// for (table = 0; table <= 10; table++) {
+//   console.log(`9 * ${table} = ${9 * table}`);
+// }
+
+/***
+
+Subtask-1:
+
+Display sum of all the odd numbers from 91 to 129.
+
+ */
+// let total = 0;
+// for (let i = 91; i < 129; i++) {
+//   if (i % 2 !== 0) {
+//     console.log(`Hi this is Odd number ${i}`);
+//     total += i;
+//   }
+// }
+// console.log(`this is the total of 91 to 129 = ${total}`);
+
+/***
+
+Subtask-2:
+
+Display sum of all the even numbers from 51 to 85.
+
+ */
+// let evenTotal = 0;
+// for (let i = 51; i < 85; i++) {
+//   if (i % 2 === 0) {
+//     console.log(`This is the even number = ${i}`);
+//     evenTotal += i;
+//   }
+// }
+// console.log(`This is the total number from 51 to 85 = ${evenTotal}`);
+/*programming hero*/
+
+/***
+
+Implement a countdown timer that counts down from 81 to 65.
+
+ */
+// for (let i = 81; i >= 65; i--) {
+//   console.log(i);
+// }
+
+/*programming hero*/
+
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+// let i = 61;
+// while (i <= 100) {
+//   if (i % 2 !== 0) {
+//     console.log(i);
+//   }
+//   i++;
+// }
+
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
+
+// let i = 78;
+// while (i <= 98) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+//   i++;
+// }
+
+/***
+
+Subtask-1:
+
+Display sum of all the odd numbers from 81 to 131.
+
+ */
+
+// let i = 81;
+// let total = 0;
+// while (i <= 131) {
+//   if (i % 2 !== 0) {
+//     console.log(`this is odd number ${i}`);
+//     total += i;
+//   }
+//   i++;
+// }
+// console.log(`This is total number from 81 to 131 = ${total}`);
+/***
+
+Subtask-2:
+
+Display sum of all the even numbers from 206 to 311.
+
+ */
+// let i = 206;
+// let total = 0;
+// while (i <= 311) {
+//   if (i % 2 === 0) {
+//     console.log(`This is even number ${i}`);
+//     total += i;
+//   }
+//   i++;
+// }
+// console.log(total);
+
+/***
+
+As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+
+ */
+
+// let i = 0;
+// while (i <= 10) {
+//   console.log(`5 * ${i} = ${5 * i}`);
+//   i++;
+// }
+
+/***
+
+Implement a countdown timer that counts down from 21 to 15.
+
+ */
+// let i = 21;
+// while (i >= 15) {
+//   console.log(i);
+//   i--;
+// }
+
+/* 
+
+Write a loop 1 to 200. Use break to exit the loop once you find 100.
+
+*/
+
+// for (i = 1; i <= 200; i++) {
+//   if (i > 100) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+/* 
+Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
+*/
+
+// let i = 1;
+
+// while (i < 200) {
+//   if (i > 100) {
+//     break;
+//   }
+//   console.log("while loop", i);
+//   i++;
+// }
+
+/* 
+Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
+*/
+// for (i = 1; i < 100; i++) {
+//   if (i % 2 === 0) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+/* 
+Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
+*/
+// for (i = 1; i < 100; i++) {
+//   let root = Math.sqrt(i);
+//   if (Number.isInteger(root) && i !== 1) {
+//     console.log(`Found the first square Number ${i}`);
+//   }
+// }
+
+/* 
+Write a loop to print even numbers from 1 to 40. Use continue to skip odd numbers.
+*/
+
+// for (let i = 1; i <= 40; i++) {
+//   if (i % 2 !== 0) {
+//     continue;
+//   }
+//   console.log(`This is even number ${i}`);
+// }
+
+/*
+display odd number from 55 to 85 and skip the numbers divisible by 5.
+*/
+
+// for (let i = 55; i <= 85; i++) {
+//   if (i % 5 === 0) {
+//     continue;
+//   } else if (i % 2 !== 0) {
+//     console.log(`This is odd number ${i}`);
+//   }
+// }
