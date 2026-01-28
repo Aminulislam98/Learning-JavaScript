058c43ce7756b0d5e3eb69ddff5fa4a8bac602cb
Aminul islam <aminurrohman98@gmail.com>
2026-01-28 21:05:52 +0000

    I finished the task of array looping

--- array.js
+++ array.js
@@ -508,3 +508,135 @@ Capitalize Every first Letter of each word in a String
 // }
 // let finalResult = results.join(" ");
 // console.log(finalResult);
+
+// Task 1
+
+// Write a JavaScript code to reverse the array colors without using the reverse method.
+
+// Input: const colors = ['red', 'blue', 'green', 'yellow', 'orange']
+
+// Output:
+
+// ['orange', 'yellow', 'green', 'blue', 'red']
+// const colors = ["red", "blue", "green", "yellow", "orange"];
+// let finalColor = [];
+// for (const color of colors) {
+//   finalColor.unshift(color);
+// }
+// console.log(finalColor);
+
+/*
+Task 2
+
+Write a JavaScript code to get the even numbers from an array using any looping technique.
+
+Input: const numbers = [12, 98, 5, 41, 23, 78, 46];
+
+Output:
+
+[12, 98, 76, 46]
+*/
+
+// const numbers = [12, 98, 5, 41, 23, 78, 46];
+// let evenNumber = [];
+// for (const number of numbers) {
+//   if (number % 2 === 0) {
+//     evenNumber.push(number);
+//   }
+// }
+// console.log(evenNumber);
+/* 
+
+Task 3
+
+Use a for...of loop to concatenate all the elements of an array into a single string.
+
+Input: var numbers = ['Tom', 'Tim', 'Tin', 'Tik']
+
+Output:
+
+'TomTimTinTik'
+*/
+
+// var numbers = ["Tom", "Tim", "Tin", "Tik"];
+// let singleString = "";
+// for (const number of numbers) {
+//   singleString += number;
+// }
+// console.log(singleString);
+
+/* 
+Task 4 (Hard)
+
+Reverse the words of a sentence. Only the position of the word will be reversed. check out the output
+
+Input: const statement = 'I am a hard working person'
+
+Output:
+
+'person working hard a am I'
+*/
+
+// let statement = "I am a hard working person";
+// let stringStatement = statement.split(" ");
+// let reverseWord = [];
+
+// for (const stringStatementReverse of stringStatement) {
+//   reverseWord.unshift(stringStatementReverse);
+// }
+// reverseWord = reverseWord.join(" ");
+// console.log(reverseWord);
+
+// Task 5
+
+// Copy the given array into another array so that changing the copy does not affect the original.Change the first element of the copied array to 99.
+
+// Input: [1, 2, 3]
+
+// Expected Output:
+
+// Original: [1, 2, 3] Copy: [99, 2, 3]
+
+// let number = [1, 2, 3];
+// let number1 = [];
+// for (const num of number) {
+//   number1.push(num);
+// }
+// number1[0] = 99;
+// console.log("Original:", number, "Copy:", number1);
+
+// Task 6
+
+// Given an array of student objects, print each student’s name and marks.
+
+// [
+//   { name: "John", marks: 85 },
+//   { name: "Alice", marks: 90 }
+// ]
+// let student = [
+//   { name: "John", marks: 85 },
+//   { name: "Alice", marks: 90 },
+// ];
+// for (const students of student) {
+//   console.log(students.name, "Scored", students.marks);
+// }
+
+// Task 7
+
+// Given a 2D array, update the value at second row first item to 99 and print the updated array.
+
+// input:
+
+// [
+//   [1, 2],
+//   [3, 4],
+//   [5, 6]
+// ]
+
+// let array = [
+//   [1, 3],
+//   [3, 4],
+//   [5, 6],
+// ];
+// array[1][0] = 99;
+// console.log(array);
