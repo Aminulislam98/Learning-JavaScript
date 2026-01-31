8e83b073744e85d12cab80ab8ab42663a537a784
Aminul islam <aminurrohman98@gmail.com>
2026-01-31 23:46:51 +0000

    I practiced about JavaScript

--- array.js
+++ array.js
@@ -700,8 +700,8 @@ Output:
 // Task-5
 
 // Write a function called odd_even() which takes an integer value and tells whether this value is even or odd. If even return Even. If odd return Odd
-function odd_even(integerValue) {
-  return integerValue % 2 === 0 ? "Even" : "Odd";
-}
-let result = odd_even(6);
-console.log(result);
+// function odd_even(integerValue) {
+//   return integerValue % 2 === 0 ? "Even" : "Odd";
+// }
+// let result = odd_even(6);
+// console.log(result);
