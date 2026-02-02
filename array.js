e66f30befec89cd355d305a907fe1d77cde92623
Aminul islam <aminurrohman98@gmail.com>
2026-02-02 17:39:47 +0000

    I practiced about js problem task

--- array.js
+++ array.js
@@ -705,3 +705,86 @@ Output:
 // }
 // let result = odd_even(6);
 // console.log(result);
+
+// Task-1:
+// Write a function to convert temperature from Celsius to Fahrenheit.
+
+// Formula: \(F=(C\times 1.8)+32\).
+
+// function celsiusToFahrenheit(cel) {
+//   let fahrenheit = cel * 1.8 + 32;
+//   return fahrenheit;
+// }
+// let fahrenheit = celsiusToFahrenheit(12);
+// console.log(fahrenheit);
+
+// Task-2:
+
+// You are given an array of numbers. Count how many times the a number is repeated in the array.
+// sample-input: numbers = [5,6,11,12,98, 5]
+// find: 5
+// output: 2
+// sample-input:
+// numbers = [5,6,11,12,98, 5]
+// find: 25
+// output: 0
+
+// let numbers = [
+//   5, 6, 11, 12, 98, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+//   5, 5, 5, 5, 5, 5, 5,
+// ];
+// function find(numbers, find) {
+//   let result = 0;
+//   for (const num of numbers) {
+//     if (num === find) {
+//       result++;
+//     }
+//   }
+//   return result ? result : "Not Found";
+// }
+// let result = find(numbers, 12);
+// console.log(result);
+
+// Task-3:
+
+// // Write a function to count the number of vowels in a string.
+// function vowels(str) {
+//   let vowels = ["a", "e", "i", "o", "u"];
+//   let vowelCount = 0;
+//   for (const string of str.toLowerCase()) {
+//     if (vowels.includes(string)) {
+//       vowelCount++;
+//     }
+//   }
+//   return vowelCount;
+// }
+
+// let result = vowels("marjiya");
+// console.log(result);
+
+// Task-4:
+
+// Write a function to find the longest word in a given string.
+
+// sample-input: I am learning Programming to become a programmer
+
+// sample-output: Programming
+
+// function longestWord(sentence) {
+//   let words = sentence.split(" ");
+//   let longest = "";
+//   for (const word of words) {
+//     if (word.length > longest.length) {
+//       longest = word;
+//     }
+//   }
+//   return longest;
+// }
+
+// let result = longestWord("i'm learning programming to become programmer");
+// console.log(result);
+
+// ask-5:
+
+// Generate a random number between 10 to 20.
+// console.log(Math.floor(Math.random() * (20 - 10 + 1) + 10));
